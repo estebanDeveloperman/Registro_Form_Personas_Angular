3457611cd46256aeb663cf1f229a592e41b3f7b6
estebanDeveloperman <estebandeveloper6@gmail.com>
2023-03-09 22:15:45 -0500

    Primer avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { Employee } from './models/employee'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  employeeArray: Employee[] = [
+    { id: 1, name: "Ryan", country: "USA" },
+    { id: 2, name: "Angelica", country: "USA" },
+    { id: 3, name: "Joe", country: "USA" },
+  ]
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit() {
+    this.selectedEmployee.id = this.employeeArray.length + 1;
+    this.employeeArray.push(this.selectedEmployee);
+    this.selectedEmployee = new Employee();
+  }
+
 }
---
+++ src/app/models/employee.ts
@@ -0,0 +1,11 @@
+export class Employee {
+    id: number;
+    name: string;
+    country: string;
+  
+    constructor() {
+        this.id = 0;
+        this.name = "";
+        this.country = "";
+    }
+  }
